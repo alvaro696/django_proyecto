8f8a9e06505ca2de95f50f3fe52cfcfe6c5047be
alvaro696 <alvaro00.am@outlook.com>
2024-12-09 00:16:05 -0400

    continuando, espere por favor

--- cotizaciones/views.py
+++ cotizaciones/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Marcas
 
 # Create your views here.
 def index(request):
     return HttpResponse("hola mundo")
+
+def cotizar (request):
+    marcas = Marcas.objects.all()
+    return render(request, "cotizar.html",{
+        "marcas": marcas,
+    })
